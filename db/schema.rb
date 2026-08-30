60076c639b8596914623a2123aa6d7021677d94f
Chalise Dinesh <bdineshbhai@gmail.com>
2026-08-30 16:59:52 +0900

    Add Post model and user association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_08_28_145819) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_30_075403) do
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "sessions", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "ip_address"
@@ -31,5 +39,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_28_145819) do
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
+  add_foreign_key "posts", "users"
   add_foreign_key "sessions", "users"
 end
